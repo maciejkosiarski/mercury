26c4e89300dac70dc4ab5f1c994691599ff394f4
Maciej Kosiarski <maciej.kosiarski@performance-media.pl>
2021-01-01 20:53:26 +0100

    Implement dedicated video stream logger, extract getRequestHAndler method

--- src/Command/HttpVideoStreamServerCommand.php
+++ src/Command/HttpVideoStreamServerCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Command;
 
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
 use React\EventLoop\Factory;
 use React\Http\Message\Response;
 use React\Http\Middleware\LimitConcurrentRequestsMiddleware;
@@ -19,10 +20,12 @@ use function React\Promise\Stream\unwrapReadable;
 class HttpVideoStreamServerCommand extends Command
 {
     private string $videos;
+    private LoggerInterface $logger;
 
-    public function __construct(string $videos)
+    public function __construct(string $videos, LoggerInterface $videoStreamLogger)
     {
         $this->videos = $videos;
+        $this->logger = $videoStreamLogger;
         parent::__construct();
     }
 
@@ -30,7 +33,8 @@ class HttpVideoStreamServerCommand extends Command
     {
         $this->setName('app:video-stream')
             ->setDescription('Video stream server in ReactPHP')
-            ->addArgument('port', InputArgument::REQUIRED,'Port number for communicate.');
+            ->addArgument('port', InputArgument::REQUIRED,'Port number for communicate.')
+            ->addArgument('uri', InputArgument::REQUIRED);
 
         parent::configure();
     }
@@ -38,19 +42,44 @@ class HttpVideoStreamServerCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $port = (int)$input->getArgument('port');
+        $uri = $input->getArgument('uri');
+
+        $this->logger->info(sprintf('Video stream server on %s port starting', $port));
+
         $loop = Factory::create();
 
-        $filesystem = \React\Filesystem\Filesystem::create($loop);
+        $server = new Server(
+            $loop,
+            new LimitConcurrentRequestsMiddleware(10),
+            new RequestBodyBufferMiddleware(2 * 1024 * 1024),
+            $this->getRequestHandler(\React\Filesystem\Filesystem::create($loop))
+        );
 
-        $requestHandler = function (ServerRequestInterface $request) use ($filesystem) {
+        $socket = new \React\Socket\Server($uri . ':' . $port, $loop);
+        $server->listen($socket);
+
+        $output->writeln('<info>Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . '</info>');
+
+        $loop->run();
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @param \React\Filesystem\FilesystemInterface $filesystem
+     * @return \Closure
+     */
+    protected function getRequestHandler(\React\Filesystem\FilesystemInterface $filesystem): \Closure
+    {
+        return function (ServerRequestInterface $request) use ($filesystem) {
             $params = $request->getQueryParams();
             $fileName = $params['video'] ?? null;
-            $iteration = $params['iteration'] ?? null;
 
             $requestUri = $request->getUri();
             $baseUri = $requestUri->getScheme() . '://' . $requestUri->getHost() . ':' . $requestUri->getPort();
-            $body = sprintf('<h1>Video streaming server iteration: %s</h1>', $iteration);
-//            $body = sprintf('<h1>Video streaming server %s</h1><ul><li><a href="%s">Lucyna</a></li></ul>',date('h:i:s A'), sprintf('%s?video=lucyna', $baseUri));
+
+            $html = '<h1>Video streaming server %s</h1><ul><li><a href="%s">Lucyna</a></li></ul>';
+            $body = sprintf($html, date('h:i:s A'), sprintf('%s?video=lucyna', $baseUri));
 
             if ($fileName === null) {
                 return new Response(200, ['Content-Type' => 'text/html'], $body);
@@ -65,26 +94,10 @@ class HttpVideoStreamServerCommand extends Command
                         return new Response(200, ['Content-Type' => 'video/mp4'], unwrapReadable($file->open('r')));
                     },
                     function () use ($baseUri) {
-                        $html = sprintf('<h1>Video streaming server</h1><p>This video doesn\'t exist on server. <a href="%s">Back</a></p>', $baseUri);
+                        $html = sprintf('<h1>Video streaming server %s</h1><p>This video doesn\'t exist on server. <a href="%s">Back</a></p>', date('h:i:s A'), $baseUri);
                         return new Response(404, ['Content-Type' => 'text/html'], $html);
                     }
                 );
         };
-
-        $server = new Server(
-            $loop,
-            new LimitConcurrentRequestsMiddleware(10),
-            new RequestBodyBufferMiddleware(2 * 1024 * 1024),
-            $requestHandler
-        );
-
-        $socket = new \React\Socket\Server('0.0.0.0:'. $port, $loop);
-        $server->listen($socket);
-
-        $output->writeln('<info>Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . '</info>');
-
-        $loop->run();
-
-        return Command::SUCCESS;
     }
 }
