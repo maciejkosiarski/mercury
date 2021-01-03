2da98a6f7ab15c544db7cb3bcfe831c2f86df83a
Maciej Kosiarski <maciej.kosiarski@performance-media.pl>
2021-01-03 21:21:40 +0100

    Add lock to http server command

--- src/Command/HttpServerV1Command.php
+++ src/Command/HttpServerV1Command.php
@@ -32,7 +32,7 @@ class HttpServerV1Command extends Command
     {
         $this->setName('app:http-server:v1')
             ->setDescription('HTTP "Hello World" server in AMP library')
-            ->addArgument('port', InputArgument::REQUIRED,'Port number for communicate.')
+            ->addArgument('port', InputArgument::REQUIRED, 'Port number for communicate.')
             ->addArgument('uri', InputArgument::REQUIRED);
 
         parent::configure();
@@ -42,28 +42,46 @@ class HttpServerV1Command extends Command
     {
         $port = (int)$input->getArgument('port');
         $uri = $input->getArgument('uri');
+
+        if (!$this->lock(sprintf('lock://%s:%s', $uri, $port))) {
+            return Command::SUCCESS;
+        }
+
+        $port = (int)$input->getArgument('port');
+        $uri = $input->getArgument('uri');
         $logger = $this->logger;
 
-        Loop::run(function () use ($port, $uri, $logger) {
-            $servers = [
-                Server::listen(sprintf('%s:%s', $uri, $port)),
-                Server::listen(sprintf('[::]:%s', $port)),
-            ];
+        try {
+            Loop::run(function () use ($port, $uri, $logger) {
+                $servers = [
+                    Server::listen(sprintf('%s:%s', $uri, $port)),
+                    Server::listen(sprintf('[::]:%s', $port)),
+                ];
 
-            $server = new HttpServer($servers, new CallableRequestHandler(static function () {
-                return new \Amp\Http\Server\Response(Status::OK, [
-                    "content-type" => "text/html; charset=utf-8"
-                ], "<h1>Amp http server: Hello, World!</h1>");
-            }), $logger);
+                $server = new HttpServer($servers, new CallableRequestHandler(static function () {
+                    return new \Amp\Http\Server\Response(Status::OK, [
+                        "content-type" => "text/html; charset=utf-8"
+                    ], "<h1>Amp http server: Hello, World!</h1>");
+                }), $logger);
 
-            yield $server->start();
+                yield $server->start();
 
-            // Stop the server when SIGINT is received (this is technically optional, but it is best to call Server::stop()).
-            Loop::onSignal(\SIGINT, static function (string $watcherId) use ($server) {
-                Loop::cancel($watcherId);
-                yield $server->stop();
+                // Stop the server when SIGINT is received (this is technically optional, but it is best to call Server::stop()).
+                Loop::onSignal(\SIGINT, static function (string $watcherId) use ($server) {
+                    Loop::cancel($watcherId);
+                    yield $server->stop();
+                });
             });
-        });
+        } catch (\Exception $e) {
+            $this->logger->warning('Exception occurred!', [
+                'msg' => $e->getMessage(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+            ]);
+            return Command::FAILURE;
+        } finally {
+            $this->release();
+        }
 
         return Command::SUCCESS;
     }
